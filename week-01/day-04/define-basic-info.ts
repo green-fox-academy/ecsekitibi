60a0f2b784dbe1e1a3ae3d8a55e91dbf1bd7e72f
Tibor Ecseki <ecseki.tibor@gmail.com>
2018-08-03 00:11:43 +0200

    adding exercises til cuboid

---
+++ week-01/day-04/define-basic-info.ts
@@ -0,0 +1,16 @@
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+let Name: string = 'Tibor Ecseki';
+let age: number = 33;
+let height: number = 1.98;
+let married: boolean = false;
+
+console.log(Name);
+console.log(age);
+console.log(height);
+console.log(married);
