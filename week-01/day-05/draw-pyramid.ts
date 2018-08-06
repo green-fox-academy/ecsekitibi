3a16c505a5232838e8995c3bb04e1cdc5977bc4b
Tibor Ecseki <ecseki.tibor@gmail.com>
2018-08-06 14:51:57 +0200

    added week02 day01 progress first two exercises

--- week-01/day-05/draw-pyramid.ts
+++ week-01/day-05/draw-pyramid.ts
@@ -13,15 +13,17 @@ let lineCount: number = 4;
 //
 // The pyramid should have as many lines as lineCount is
 
-let space = ' ';
-let star = '*';
+let space : string = '';
+let star : string = '*';
 
 for (let i = 0; i < lineCount; i++) {
     for (let j = i; j < lineCount; j++) {
         space += ' ';
     }
     console.log( space + star );
-    space = ' ';
     star += '**';
+    space = '';
     
 }
+
+
