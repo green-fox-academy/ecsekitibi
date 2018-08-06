1413ab17be6a76498ff0b5e7735574ea755263c0
Tibor Ecseki <ecseki.tibor@gmail.com>
2018-08-06 16:37:31 +0200

    added factorio

---
+++ week-02/day-01/factorio.ts
@@ -0,0 +1,13 @@
+'use strict'
+export {}
+// -  Create a function called `factorio`
+//    that returns it's input's factorial
+
+function factorio(params:number) {
+    let numbers = 1; 
+    for (let i=1; i <=params; i++) {
+        numbers = i * numbers;
+    }
+    return numbers;
+}
+console.log(factorio(6));
