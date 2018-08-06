6ea1038197e7f95b6f4c558911065f7747c53340
Tibor Ecseki <ecseki.tibor@gmail.com>
2018-08-06 17:34:38 +0200

    added print-params and third excercises

---
+++ week-02/day-01/print-params.ts
@@ -0,0 +1,14 @@
+'use strict'
+export {}
+// -  Create a function called `printParams`
+//    which logs to the console the input parameters
+//    (can have multiple number of arguments)
+
+let numbers = [10,87,3,4,5,6];
+
+function printParams(params:number[]) {
+    for (let i = 0; i < params.length; i++) {
+    console.log(params[i]);
+    }   
+}   
+printParams(numbers);
