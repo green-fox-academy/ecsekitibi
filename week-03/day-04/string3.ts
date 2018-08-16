9a31d78ec31cf70268e4d4803c20b4dd7e1b1535
Tibor Ecseki <ecseki.tibor@gmail.com>
2018-08-16 21:59:42 +0200

    added string3 and fibonacchi excercises to my daily progress as home practice

--- week-03/day-04/string3.ts
+++ week-03/day-04/string3.ts
@@ -1,6 +1,15 @@
-
 // Given a string, compute recursively a new string where all the
 // adjacent chars are now separated by a '*'.
 
 'use strict';
 export { };
+
+function adjacentChars(newString: string): any {
+    if (newString.length > 0) {
+        return newString.slice(0, 1) + '*' + adjacentChars(newString.slice(1));
+    } else {
+        return '';
+    }
+}
+
+console.log(adjacentChars('nem tudom megcsinalja-e'));
