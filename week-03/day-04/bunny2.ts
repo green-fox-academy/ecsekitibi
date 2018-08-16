8f8cf27055924157b6c1cc7985ee5e85453e407a
Tibor Ecseki <ecseki.tibor@gmail.com>
2018-08-16 15:34:42 +0200

    added bunny1 & bunny2 excercise

---
+++ week-03/day-04/bunny2.ts
@@ -0,0 +1,17 @@
+'use strict'
+export { }
+// We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
+// (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
+// have 3 ears, because they each have a raised foot. Recursively return the
+// number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+function earsOfBunnies(n: number) {
+    if (n % 2 == 0) {
+        return 3 + earsOfBunnies(n - 1);
+    } else if (n % 2 == 1) {
+        return 2 + earsOfBunnies(n - 1);
+    } else {
+        return 0;
+    }
+}
+console.log(earsOfBunnies(10));
