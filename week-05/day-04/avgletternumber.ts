691b9cb3eefef3953342fa8148aece9d57eba89e
Tibor Ecseki <ecseki.tibor@gmail.com>
2018-09-13 10:34:31 +0200

    add package.json

--- week-05/day-04/avgletternumber.ts
+++ week-05/day-04/avgletternumber.ts
@@ -1,21 +1,27 @@
+'use strict';
+declare function require(path: string): any;
+const fs = require('fs');
+export { }
+
 // Count the average letter number of the words in a text
-function averageLetterNumber (inputFile: string): void {
-  let readFromFile: string = fs.readFileSync(inputFile, charEncoding);
+function averageLetterNumber(inputFile: string): void {
+  let readFromFile: string = fs.readFileSync(inputFile, 'utf-8');
   let fileWithoutSpecChar: string = readFromFile.replace(/[^a-zA-Z]/gi, ' ');
   //console.log(fileWithoutSpecChar);
-  let fileToArray: string [] = fileWithoutSpecChar.split(' ');
+  let fileToArray: string[] = fileWithoutSpecChar.split(' ');
   //console.log(fileToArray);
-  let newFileArray: string [] = [];
+  let newFileArray: string[] = [];
   fileToArray.forEach((element) => {
     if (element !== '') {
       newFileArray.push(element);
     }
   });
+  //console.log(newFileArray);
   let counter: number = 0;
   newFileArray.forEach((element) => {
     counter += element.length;
   });
-  console.log( counter / newFileArray.length);
+  console.log(counter / newFileArray.length);
 }
 
-averageLetterNumber(fileToRead);
+averageLetterNumber('practice.txt');
